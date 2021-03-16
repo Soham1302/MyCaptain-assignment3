3e493f407b2cdfed6719e8d6584ea200514e3691
Soham1302 <80255611+Soham1302@users.noreply.github.com>
2021-03-16 18:07:25 +0530

    Add files via upload

---
+++ assignment 3 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+ main()
+{
+	cout<<"Enter Elements:";
+	
+	int array[4];
+	int a;
+	array[0]=a;
+	cin>>a;
+		int b;
+	 array[1]=b;
+	cin>>b;
+		int c;
+	 array[2]=c;
+	cin>>c;
+		int d;
+	 array[3]=d;
+	cin>>d;
+		int e;
+	array[4]=e;
+	cin>>e;
+	
+	cout<<"\nYou Entered:";
+	cout<<a<<"\n";
+	cout<<b<<"\n";
+	cout<<c<<"\n";
+	cout<<d<<"\n";
+	cout<<e<<"\n";
+	
+	return 0;
+}
